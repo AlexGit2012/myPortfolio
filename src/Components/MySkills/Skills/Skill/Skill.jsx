f59d8399e51a492f3cc087e0b9d7c120ed5b4b18
AlexGit2012 <a.karavaychik@gmail.com>
2020-02-17 22:48:39 +0300

    - added icons for skills

--- src/Components/MySkills/Skills/Skill/Skill.jsx
+++ src/Components/MySkills/Skills/Skill/Skill.jsx
@@ -4,8 +4,9 @@ import React from "react";
 export const Skill = (props) => {
     return <div className={styles.element}>
         <div className={styles.skillContainer}>
-            <div className={styles.skillImg}></div>
-            <br/>
+            <div className={styles.skillImg}>
+                <img src={props.icon} width="45px" alt="Skill icon"/>
+            </div>
             {props.skillName}
             <br/>
             <br/>
--- src/Components/MySkills/Skills/Skills.jsx
+++ src/Components/MySkills/Skills/Skills.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import styles from "./Skills.module.css";
 import {HeaderOfBlock} from "../../Other/HeaderOfBlock/HeaderOfBlock";
 import {Skill} from "./Skill/Skill";
+import reactIcon from "../../../assets/icons/icons8-react-native.svg";
+import reduxIcon from "../../../assets/icons/redux_4691205.png"
+import htmlIcon from "../../../assets/icons/iconfinder_10-html5_104494.ico"
+import cssIcon from "../../../assets/icons/css-3-logo.svg"
+import javascriptIcon from "../../../assets/icons/iconfinder_js_282802.ico"
+
 
 const Skills = () => {
     return (
@@ -9,9 +15,11 @@ const Skills = () => {
             <HeaderOfBlock headerText={"Мои навыки"}/>
             <div className={styles.middleContainer}>
                 <div className={styles.elements}>
-                    <Skill skillName={"React JS"} skillDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacinia purus. Sed arcu metus, scelerisque at nisi eget, hendrerit commodo odio."}/>
-                    <Skill skillName={"HTML & CSS"} skillDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacinia purus. Sed arcu metus, scelerisque at nisi eget, hendrerit commodo odio."}/>
-                    <Skill skillName={"JavaScript"} skillDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacinia purus. Sed arcu metus, scelerisque at nisi eget, hendrerit commodo odio."}/>
+                    <Skill icon={reactIcon} skillName={"React JS"} skillDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacinia purus. Sed arcu metus, scelerisque at nisi eget, hendrerit commodo odio."}/>
+                    <Skill icon={reduxIcon} skillName={"Redux"} skillDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacinia purus. Sed arcu metus, scelerisque at nisi eget, hendrerit commodo odio."}/>
+                    <Skill icon={htmlIcon} skillName={"HTML"} skillDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacinia purus. Sed arcu metus, scelerisque at nisi eget, hendrerit commodo odio."}/>
+                    <Skill icon={cssIcon} skillName={"CSS"} skillDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacinia purus. Sed arcu metus, scelerisque at nisi eget, hendrerit commodo odio."}/>
+                    <Skill icon={javascriptIcon} skillName={"JavaScript"} skillDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacinia purus. Sed arcu metus, scelerisque at nisi eget, hendrerit commodo odio."}/>
                 </div>
             </div>
 
