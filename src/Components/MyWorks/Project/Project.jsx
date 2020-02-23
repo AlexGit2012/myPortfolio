807bae7a0495365ff5d80f887bd909319b14db72
AlexGit2012 <a.karavaychik@gmail.com>
2020-02-23 18:58:49 +0300

    - work with background images in Project components

--- src/Components/MyWorks/Project/Project.jsx
+++ src/Components/MyWorks/Project/Project.jsx
@@ -4,12 +4,13 @@ import React from "react";
 export const Project = (props) => {
     return <div className={styles.myWorkElement}>
         <div className={styles.workImg}>
-            <button className={styles.myWorkButton}>Смотреть</button>
+            <button className={styles.myWorkButton}>Смотреть проект</button>
+            <button className={styles.myWorkButton}>Просмотр кода</button>
         </div>
         <div className={styles.workDescription}>
             <div className={styles.workDescriptionElement}>
                 <div className={styles.workDescriptionName}>Название проекта</div>
-                <div className={styles.workDescriptionText}>Краткое описание</div>
+                <div className={styles.workDescriptionText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacinia purus.</div>
             </div>
         </div>
     </div>
