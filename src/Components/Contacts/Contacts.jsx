46c275c99108c0f5bf0b2cedea139d056f347866
AlexGit2012 <a.karavaychik@gmail.com>
2020-03-02 17:06:38 +0300

    - changed size of contact block elements and added new styles

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import styles from "./Contact.module.css";
 import {Field} from "redux-form";
+import {HeaderOfBlock} from "../Other/HeaderOfBlock/HeaderOfBlock";
 
 const Contacts = () => {
     return <div className={styles.contacts}>
             <div className={styles.container}>
-                <div className={styles.contactHeader}>Контакты</div>
+                <HeaderOfBlock headerText="Контакты"/>
                 <form className={styles.formFields}>
                     <div className={styles.contactInputFields}>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <textarea name="" id="" cols="30" rows="10"/>
+                        <input type="text" placeholder="Имя" className={styles.inputArea}/>
+                        <input type="email"placeholder="E-mail" className={styles.inputArea}/>
+                        <textarea name="" id="" cols="30" rows="10"
+                                  placeholder="Сообщение"
+                                  className={styles.messageArea}/>
                     </div>
-                        <button>Click</button>
+                        <button className={styles.formButton}>Отправить</button>
                 </form>
             </div>
         </div>
