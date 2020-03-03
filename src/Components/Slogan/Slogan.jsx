88a595ce71859015df46855a2da8478f928a7930
AlexGit2012 <a.karavaychik@gmail.com>
2020-03-03 17:43:52 +0300

    - just start work with buttons in contact and slogan form

--- src/Components/Slogan/Slogan.jsx
+++ src/Components/Slogan/Slogan.jsx
@@ -4,6 +4,7 @@ import styles from "./Slogan.module.css";
 const Slogan = () => {
     return <div className={styles.slogan} >
         <div className={styles.hireMeSlogan}>Рассматриваю варианты удаленной работы</div>
+        <button className={styles.hireButton}>Нанять меня</button>
     </div>
 }
 
