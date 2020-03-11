5f201a7c71c9b13aaf5bd1b096c1ac7e0b2a076e
AlexGit2012 <a.karavaychik@gmail.com>
2020-03-11 14:51:24 +0300

    - added animation menu, react-reveal used

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import styles from "./Header.module.css";
 import NavMenu from "./NavMenu/NavMenu";
 import Menu from "../Other/Menu/Menu";
+import Fade from 'react-reveal/Fade';
 
 const Header = () => {
     return <div>
-        <div className={styles.header}>
-            <div className={styles.container}>
-                <NavMenu/>
-            </div>
+        <div className={styles.menuBtn}>
+            <Menu/>
+            <Fade duration={500} top={true}>
+                <div className={styles.hiddenContainer}>
+                    <div className={styles.header}>
+                        <div className={styles.container}>
+                            <NavMenu/>
+                        </div>
+                    </div>
+                </div>
+            </Fade>
         </div>
-        <Menu className={styles.menuBtn}/>
     </div>
-
 
 
 }
